5897ae9677866cc1158908361cb57e90b998d2c3
pringle97 <malia.pringle@gmail.com>
2021-12-13 21:35:25 -0800

    adding code comments

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,12 +4,12 @@
 //POST / api / notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 //DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
-
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 
+//function getting notes
 router.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) { console.log(err) }
@@ -17,23 +17,25 @@ router.get('/api/notes', (req, res) => {
   })
 })
 
+//function posting notes to JSON file
 router.post('/api/notes', (req, res) => {
-  const newNote = req.body
+  const note = req.body
   fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) { console.log(err) }
-    const notes = JSON.parse(data)
-    notes.push(item)
-    fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
+    const newNotes = JSON.parse(data)
+    newNotes.push(note)
+    fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(newNotes), err => {
       if (err) { console.log(err) }
       res.sendStatus(200)
     })
   })
 })
 
+//function for deleting notes
 router.delete('/api/notes/:id', (req, res) => {
   fs.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
     const deletedNotes = JSON.parse(data)
-    for (let i=0; i < deletedNotes.length; i++) {
+    for (let i = 0; i < deletedNotes.length; i++) {
       const dbElement = deletedNotes[i]
       if (dbElement.id === req.params.id) {
         deletedNotes.splice(i, 1)
@@ -41,7 +43,7 @@ router.delete('/api/notes/:id', (req, res) => {
     }
     fs.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
       if (err) { console.log(err) }
-      res.sendStatus(200) 
+      res.sendStatus(200)
     })
   })
 })
