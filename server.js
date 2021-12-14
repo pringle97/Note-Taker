3a29de96446da2e13cc1297c50e66bce172bc796
pringle97 <malia.pringle@gmail.com>
2021-12-14 10:33:55 -0800

    adding functions for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const router = require('express').Router()
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +8,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(require('./routes/itemRoutes.js'))
+
+//sends notes file to notes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+
+//sends index file to *
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//port for heroku
+const port = process.env.PORT || 4000;
+
+app.listen(port);
