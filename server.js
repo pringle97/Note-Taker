a13483bbb8112b8e26845700c04a76b18d4fef66
pringle97 <malia.pringle@gmail.com>
2021-12-14 10:15:49 -0800

    app.use

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./routes/itemRoutes.js'))
