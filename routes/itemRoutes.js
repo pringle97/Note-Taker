fcca10d4962769fcee093e15f9629471272d3675
pringle97 <malia.pringle@gmail.com>
2021-12-15 20:52:27 -0800

    editing code

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -7,7 +7,6 @@
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
-const { v4: uuidv4 } = require('uuid')
 
 //function getting notes
 router.get('/api/notes', (req, res) => {
